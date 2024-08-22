de82b2230be00474f3883e82c9f96e6f3f0865a6
Mukitul Islam Mukit <hellomukit@gmail.com>
2024-08-23 03:10:38 +0600

    feat: update install script

--- install.sh
+++ install.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
+# INFO: check if sudo is available
+if command -v sudo &>/dev/null; then
+  SUDO="sudo"
+else
+  SUDO=""
+fi
+
 # INFO: Install ansible
 
 if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
   echo -e "\n🚀 Adding Ansible PPA"
-  sudo apt-add-repository ppa:ansible/ansible -y
+  $SUDO apt-add-repository ppa:ansible/ansible -y
 fi
 
 if ! hash ansible >/dev/null 2>&1; then
   echo -e "\n🚀 Installing Ansible..."
-  sudo apt update
-  sudo apt install software-properties-common ansible git python-apt -y
+  $SUDO apt update
+  $SUDO apt install software-properties-common build-essential ansible git curl -y
 else
   echo -e "\n✅ Ansible already installed"
+fi
+
+# INFO: Clone repository
+
+cd ~ || exit
+
+if [ ! -d "ansible-server" ]; then
+  echo -e "\n🚀 Cloning repository..."
+  git clone https://github.com/mimukit/ansible-server.git
+  cd ansible-server || exit
 fi
 
 # INFO: Run ansible playbook
